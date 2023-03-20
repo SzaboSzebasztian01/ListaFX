66cc77b86944199ac9e853716145013c3dc2c8b9
Szabó Szebasztián <szaboszebasztianlevente@ktch.hu>
2023-03-20 16:39:59 +0100

    first commit

---
+++ src/views/ComboPanel.java
@@ -0,0 +1,13 @@
+package views;
+
+import javafx.scene.control.ComboBox;
+
+public class ComboPanel extends ComboBox<String> {
+
+    public ComboPanel() {
+        this.getItems().add("Déli");
+        this.getItems().add("Hazai");
+
+    }
+    
+}
---
+++ src/views/ListPanel.java
@@ -0,0 +1,13 @@
+package views;
+
+import javafx.scene.control.ListView;
+
+public class ListPanel extends ListView<String> {
+
+    public ListPanel() {
+        this.getItems().add("ly");
+        this.getItems().add("na");
+
+    }
+    
+}
